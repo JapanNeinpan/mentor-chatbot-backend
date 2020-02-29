f17ad4f90957c629dd6b575b67757c4347c41562
Pan <hypersausage@web.de>
2020-02-29 20:03:24 +0100

    Create questions.js

---
+++ routes/questions.js
@@ -0,0 +1,41 @@
+const express   = require('express');
+const Question  = require('../models/Question');
+const router = express.Router();
+
+// GET ALL QUESTIONS
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Question.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({message: err})
+    }
+});
+
+// GET A SPECIFIED QUESTION
+router.get('/:questionId', async (req, res) => {
+    try {
+        const post = await Question.find(req.params.questionId);
+        res.json(post);
+    } catch (err) {
+        res.json({message: err})
+    }
+});
+
+//SUBMIT A QUESTION
+router.post('/', async (req, res) => {
+
+    const question = new Question({
+        question: req.body.question,
+        author: req.body.author
+    });
+
+    try {
+        const submittedQuestion = await question.save();
+        res.json(submittedQuestion);
+    } catch (err) {
+        res.json({message: err})
+    }
+});
+
+module.exports = router;
